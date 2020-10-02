ef9fc37157040c32023bf011433b00a64679298c
jaopol <jaopol@gmail.com>
2020-10-01 21:17:01 -0300

    Primeiro commit

---
+++ src/main/java/br/com/contabancaria/ContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.contabancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/contabancaria/ContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contabancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
